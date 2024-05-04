b5c29b6c407f147d5b4b559e18fb933f39c7d760
İlker  Yelekçioğlu <ilker.yelekcioglu@eveshop.com.tr>
2024-05-04 16:59:36 +0300

    language file is created and fixed problems

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,6 +1,9 @@
-import { useEffect, useState } from "react";
-import Axios, { AxiosError } from "axios";
+import { useEffect, useMemo, useState } from "react";
 import { signUp } from "./api";
+import { Input } from "./components/Input";
+import { useTranslation } from "react-i18next";
+import { axiosError } from "axios";
+import { LanguageSelector } from "../../shared/components/LanguageSelector";
 
 export function SignUp() {
   const [username, setUsername] = useState(); //Bu bir React Hook
@@ -10,12 +13,38 @@ export function SignUp() {
   const [apiProgress, setApiProgress] = useState(false);
   const [successMessage, setSuccessMessage] = useState();
   const [errors, setErrors] = useState({});
+  const [generalError, setGeneralError] = useState();
 
-  //useEffect hook kısmını kullan
+  //useEffect hook kısmını kullan.React hooklara bak
+
+  const { t } = useTranslation();
 
   useEffect(() => {
-    setErrors({});
+    setErrors(function (lastErrors) {
+      return {
+        ...lastErrors,
+        username: undefined,
+      };
+    });
   }, [username]);
+
+  useEffect(() => {
+    setErrors(function (lastErrors) {
+      return {
+        ...lastErrors,
+        email: undefined,
+      };
+    });
+  }, [email]);
+
+  useEffect(() => {
+    setErrors(function (lastErrors) {
+      return {
+        ...lastErrors,
+        password: undefined,
+      };
+    });
+  }, [password]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -28,17 +57,27 @@ export function SignUp() {
         password,
       });
       setSuccessMessage(response.data.message);
-    } catch (AxiosError) {
+    } catch (axiosError) {
       if (
-        AxiosError.response?.data &&
-        AxiosError.response.data.status === 400
+        axiosError.response?.data &&
+        axiosError.response.data.status === 400
       ) {
-        setErrors(AxiosError.response.data.validationErrors);
+        setErrors(axiosError.response.data.validationErrors);
+      } else {
+        setGeneralError(t("genericError"));
       }
     } finally {
       setApiProgress(false);
     }
   };
+
+  const passwordRepeatError = useMemo(() => {
+    if (password && password !== passwordRepeat) {
+      console.log("always running");
+      return t("passwordMismatch");
+    }
+    return "";
+  }, [password, passwordRepeat]);
 
   /*
 
@@ -47,58 +86,37 @@ fonksiyona dışarıdan erişilebilmesi için export kullanımı gerekmektedir.
   return (
     <div className="container">
       <div className="col-lg-6 offset-lg-3 col-sm-8 offset-sm-2">
-        xw
         <form className="card" onSubmit={onSubmit}>
           <div className="text-center card-header">
-            <h1>SignUp</h1>
+            <h1>{t("signUp")}</h1>
           </div>
           <div className="card-body">
-            <div className="mb-3">
-              <label htmlFor="username" className="form-label">
-                Username
-              </label>
-              <input
-                id="username"
-                className={
-                  errors.username ? "form-control is-invalid" : "form-control"
-                }
-                onChange={(event) => setUsername(event.target.value)}
-              />
-              <div className="invalid-feedback">{errors.username}</div>
-            </div>
-            <div>
-              <label htmlFor="email" className="form-label">
-                Email
-              </label>
-              <input
-                id="email"
-                className="form-control"
-                onChange={(event) => setEmail(event.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="form-label">
-                Password
-              </label>
-              <input
-                id="password"
-                className="form-control"
-                type="password"
-                onChange={(event) => setPassword(event.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="passwordRepeat" className="form-label">
-                Password Repeat
-              </label>
-              <input
-                id="passwordRepeat"
-                className="form-control"
-                type="password"
-                onChange={(event) => setPasswordRepeat(event.target.value)}
-              />
-            </div>
-
+            <Input
+              id="username"
+              label={t("username")}
+              errors={errors.username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+            <Input
+              id="email"
+              label={t("email")}
+              errors={errors.email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <Input
+              id="password"
+              label={t("password")}
+              error={errors.password}
+              onChange={(event) => setPassword(event.target.value)}
+              type="password"
+            />
+            <Input
+              id="passwordRepeat"
+              label={t("passwordRepeat")}
+              error={errors.passwordRepeat}
+              type="password"
+              onChange={(event) => setPasswordRepeat(event.target.value)}
+            />
             {successMessage && <div>{successMessage}</div>}
 
             <div className="text-center">
@@ -119,6 +137,7 @@ fonksiyona dışarıdan erişilebilmesi için export kullanımı gerekmektedir.
             </div>
           </div>
         </form>
+        <LanguageSelector />
       </div>
     </div>
   );
