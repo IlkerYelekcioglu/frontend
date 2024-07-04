aa126ee9e5fa49d345942dc94454c311a12d90d2
İlker  Yelekçioğlu <ilker.yelekcioglu@eveshop.com.tr>
2024-07-04 22:05:49 +0300

    global state management is finished

--- src/shared/state/redux.js
+++ src/shared/state/redux.js
@@ -1,4 +1,4 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit ";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { loadAuthState, storeAuthState } from "./storage";
 
 const authSlice = createSlice({
